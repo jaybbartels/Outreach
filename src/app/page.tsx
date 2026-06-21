6871d00383de4232567132a38209c37310196568
Jay Bartels <jaybartels@me.com>
2026-06-21 15:23:47 -0700

    test: Simplify page to debug 404 issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from 'react'
-import CompanyInput from '@/components/CompanyInput'
-
 export const dynamic = 'force-dynamic'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <Suspense fallback={<div className="p-6">Loading...</div>}>
-        <CompanyInput />
-      </Suspense>
+    <main>
+      <h1>Hello World</h1>
+      <p>If you see this, the app is working!</p>
     </main>
   )
 }
